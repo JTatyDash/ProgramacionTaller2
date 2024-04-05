34c41bdd7f9e2934c9050df2840b243dd07b1f37
tatiana <you@example.com>
2024-04-04 20:44:58 -0500

    ejercicio1, ejercicio3, ejercicio5

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,1 +1,11 @@
-//Generar un código que determine si un numero es par o impar
+//Generar un código que determine si un numero es par o impar.
+const dataFromConsole = process.argv;
+
+const number = +dataFromConsole[2];
+
+if (number %2 == 0){
+    console.log("El numero es par ", number);
+}
+else{
+    console.log("El numero es impar", number);
+}
---
+++ ejercicio3/ejercicio3.js
@@ -0,0 +1,14 @@
+//Generar un código determine la diferencia (resta) entre dos
+//números de tal manera que la resta siempre se haga del numero mayor hacia el numero menor
+
+const dataFromConsole = process.argv;
+
+const number1 = +dataFromConsole[2];
+const number2 = +dataFromConsole[3];
+
+if (number1 >= number2){
+    resta = number1 - number2;
+    console.log("El resultado de la resta es: ", resta);
+} else {
+    console.log("No es posible realizar la resta")
+}
